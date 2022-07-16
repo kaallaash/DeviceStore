c6b656f1bab51d538be045b0a57b0d9e6741ebe9
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-07-17 01:38:55 +0300

    Implement DeviceStore.Services.Users.Api

---
+++ Services/Users/DeviceStore.Services.Users.Api/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿using DeviceStore.Services.Users.Contract;
+using DeviceStore.Services.Users.Contract.Models;
+using DeviceStore.Services.Users.Contract.Models.Commands;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceStore.Services.Users.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUsersService _usersService;
+
+    public UserController(
+        IUsersService userService)
+    {
+        _usersService = userService;
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<UserDetails>> Get(
+    [FromRoute] GetUserCommand command,
+    CancellationToken cancellationToken = default)
+    {
+        var result = await _usersService
+            .Get(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<string>> Create(
+      [FromBody] CreateUserCommand command,
+      CancellationToken cancellationToken = default)
+    {
+        var result = await _usersService
+            .Create(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<string>> Update(
+      [FromBody] UpdateUserCommand command,
+      CancellationToken cancellationToken = default)
+    {
+        var result = await _usersService
+            .Update(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<string>> Delete(
+      [FromBody] DeleteUserCommand command,
+      CancellationToken cancellationToken = default)
+    {
+        await _usersService
+           .Delete(command, cancellationToken)
+           .ConfigureAwait(false);
+
+        return Ok();
+    }
+}
---
+++ Services/Users/DeviceStore.Services.Users/Registration.cs
@@ -0,0 +1,24 @@
+﻿using DeviceStore.Services.Users.Context;
+using DeviceStore.Services.Users.Contract;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeviceStore.Services.Users;
+
+public static class Registration
+{
+    public static IServiceCollection AddUsers(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContextPool<UsersDbContext>(
+            (s, b) =>
+                b.UseNpgsql(configuration.GetConnectionString("UsersDb")));
+
+        services.AddScoped<IUsersService, UsersService>();
+
+        return services;
+    }
+}
