47925380927066c9a5eb72210e6f08cd7c1d1e58
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-07-22 00:45:33 +0300

    Implement DeviceStore.Services.Warehouse.Api

---
+++ Services/Warehouse/DeviceStore.Services.Warehouse.Api/Controllers/DeviceController.cs
@@ -0,0 +1,124 @@
+﻿using DeviceStore.Services.Warehouse.Contract;
+using DeviceStore.Services.Warehouse.Contract.Models;
+using DeviceStore.Services.Warehouse.Contract.Models.Commands;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceStore.Services.Warehouse.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DeviceController : ControllerBase
+{
+    private readonly IDevicesService _devicesService;
+
+    public DeviceController(
+        IDevicesService devicesService)
+    {
+        _devicesService = devicesService;
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<DeviceDetails>> Get(
+    [FromRoute] GetDeviceDetailsCommand command,
+    CancellationToken cancellationToken = default)
+    {
+        var result = await _devicesService
+            .Get(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<DeviceDetailsScrollableCollection>> Search(
+        [FromRoute] SearchScrollableCollectionCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _devicesService
+            .Search(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<DeviceDetails>> Create(
+      [FromBody] CreateDeviceCommand command,
+      CancellationToken cancellationToken = default)
+    {
+        var result = await _devicesService
+            .Create(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<DeviceDetails>> Add(
+        [FromBody] AddDeviceCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _devicesService
+            .Add(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<DeviceDetails>> Subtract(
+        [FromBody] SubtractDeviceCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _devicesService
+            .Subtract(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<string>> Update(
+      [FromBody] UpdateDeviceCommand command,
+      CancellationToken cancellationToken = default)
+    {
+        var result = await _devicesService
+            .Update(command, cancellationToken)
+            //.WithActionResult()
+            .ConfigureAwait(false);
+
+        return result;
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<string>> Delete(
+      [FromBody] DeleteDeviceCommand command,
+      CancellationToken cancellationToken = default)
+    {
+        await _devicesService
+           .Delete(command, cancellationToken)
+           .ConfigureAwait(false);
+
+        return Ok();
+    }
+}
---
+++ Services/Warehouse/DeviceStore.Services.Warehouse.Api/Startup.cs
@@ -0,0 +1,62 @@
+﻿using NJsonSchema.Generation;
+using System.Text.Json.Serialization;
+
+namespace DeviceStore.Services.Warehouse.Api;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddWarehouse(Configuration);
+
+        services
+            .AddControllers()
+            .AddJsonOptions(
+                options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    options.JsonSerializerOptions.AllowTrailingCommas = true;
+                });
+
+        services.AddOpenApiDocument(
+            settings =>
+            {
+                settings.DocumentName = "openapi";
+                settings.SchemaGenerator.Settings.DefaultReferenceTypeNullHandling =
+                ReferenceTypeNullHandling.NotNull;
+            });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+        app.UseOpenApi(settings => settings.Path = "/swagger/{documentName}/swagger.json");
+        app.UseSwaggerUi3(
+            settings =>
+            {
+                settings.Path = "/swagger";
+                settings.DocumentPath = "/swagger/{documentName}/swagger.json";
+            });
+        app.UseReDoc(
+            settings =>
+            {
+                settings.Path = "/redoc";
+                settings.DocumentPath = "/swagger/{documentName}/swagger.json";
+            });
+    }
+}
