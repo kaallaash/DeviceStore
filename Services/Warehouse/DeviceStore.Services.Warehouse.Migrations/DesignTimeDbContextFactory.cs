73cc46e91c105ce6f6057423f3370ac98c861624
Andrey Kalashnikov <Kaallaash@gmail.com>
2022-07-22 00:25:19 +0300

    implement DeviceStore.Services.Warehouse.Migrations

---
+++ Services/Warehouse/DeviceStore.Services.Warehouse.Migrations/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using DeviceStore.Services.Warehouse.Context;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DeviceStore.Services.Warehouse.Migrations;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DevicesDbContext>
+{
+    public DevicesDbContext CreateDbContext(params string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<DevicesDbContext>();
+        optionsBuilder.UseNpgsql(
+            config.GetConnectionString("WarehouseDb"),
+            builder => builder.MigrationsAssembly(typeof(DesignTimeDbContextFactory)
+            .Assembly.GetName().Name));
+
+        return new DevicesDbContext(optionsBuilder.Options);
+    }
+}
---
+++ Services/Warehouse/DeviceStore.Services.Warehouse.Migrations/Migrations/20220721212422_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceStore.Services.Warehouse.Migrations.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Count = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_Id",
+                table: "Devices",
+                column: "Id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
